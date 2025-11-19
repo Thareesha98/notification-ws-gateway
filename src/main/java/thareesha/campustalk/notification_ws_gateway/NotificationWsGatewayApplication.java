48737d3f5ddb445b92f4da3af5615ce6874cf0d6
Thareesha <thareesha98>
2025-11-19 23:47:26 +0530

    Initiated

---
+++ src/main/java/thareesha/campustalk/notification_ws_gateway/NotificationWsGatewayApplication.java
@@ -0,0 +1,13 @@
+package thareesha.campustalk.notification_ws_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationWsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationWsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thareesha/campustalk/notification_ws_gateway/NotificationWsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package thareesha.campustalk.notification_ws_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationWsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
