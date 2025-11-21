082c4cc82b82c566452d1d418345ade936ac27a1
Thareesha <thareesha98>
2025-11-21 18:26:58 +0530

    Added eureka connectivity

--- src/main/java/thareesha/campustalk/notification_ws_gateway/NotificationWsGatewayApplication.java
+++ src/main/java/thareesha/campustalk/notification_ws_gateway/NotificationWsGatewayApplication.java
@@ -2,8 +2,10 @@ package thareesha.campustalk.notification_ws_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationWsGatewayApplication {
 
 	public static void main(String[] args) {
